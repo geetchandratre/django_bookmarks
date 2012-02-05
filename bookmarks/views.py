8b83b528ea560ca770f678843709afc253dcb6a2
Geet Chandratre <geetchandratre@gmail.com>
2012-02-05 19:46:48 +0530

    Geet | Adding logout page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,28 +4,29 @@ from django.template.loader import get_template
 from django.contrib.auth.models import User
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 
 def main_page(request):
-    template = get_template('main_page.html')
-
-    variables = Context({
-        'head_title': u'Django Bookmarks',
-        'page_title': u'Welcome to Django Bookmarks',
-        'page_body': u'Where you can store and share bookmarks!'
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+    'main_page.html',
+    {'user': request.user}
+    )
 
 def user_page(request, username):
     try:
         user = User.objects.get(username=username)
     except User.DoesNotExist:
         raise Http404(u'Requested user not found.')
-    bookmarks = user.bookmark_set.all()
-    template = get_template('user_page.html')
-    variables = Context({
-     'username': username,
-     'bookmarks': bookmarks
-    })
-    output = template.render(variables)
+        bookmarks = user.bookmark_set.all()
+        template = get_template('user_page.html')
+        variables = Context({
+        'username': username,
+        'bookmarks': bookmarks
+        })
+        output = template.render(variables)
     return HttpResponse(output)
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
--- urls.py
+++ urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     (r'^$', main_page),
     (r'^user/(\w+)/$', user_page),
     (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', logout_page),
 
 )
